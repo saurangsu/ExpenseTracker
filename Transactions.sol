afe718e52af336ae3f1a90a7591406c783f3da50
saurangsu <saurangsu.lahiri.88@gmail.com>
2019-03-07 18:44:09 -0500

    Updated Transaction.sol
    Added modifiers to make checks to add Expense

--- Transactions.sol
+++ Transactions.sol
@@ -2,10 +2,13 @@ pragma solidity ^0.5.0;
 
 import "./User.sol";
 
-contract transactions{
+contract transactions is user{
+    
+    uint transactionId = 0;
     
     enum ExpenseTypes {DEFAULT, EQUAL, PERCENTAGE, SHARE}
     ExpenseTypes expenseType;
+    
     struct expense{
         
         address[] spenders;
@@ -13,11 +16,55 @@ contract transactions{
         
     }
     
-    
     mapping(uint => expense) expenseMapper;    
+
+    //@dev: for every element in the _friends array, verify if that friend exists using FN doesFriendExist
+    modifier checkFriendList(address[] memory _friends){
+        users memory currentUser = addressToUserMapping[msg.sender];
+        uint noOfFriends = currentUser.friends.length;
+        //bool allFriendExist = false;
+        //@dev: when friends count is 0, the for loop is breaking, due to underflow of unsigned integer
+        //hence putting it in an if..else.. statement
+        if(noOfFriends == 0){
+            revert("You do not have any friends to share exp1enses with");
+        }else{
+            for(uint i=0;i<_friends.length;i++){
+                if(_friends[i] == msg.sender){
+                    _;
+                }
+                else if (doesFriendExist(_friends[i])){
+                    //allFriendExist = true;
+                    _;
+                }else{
+                    revert("User does not exist in your friend list");
+                }                            
+            }
+        }
+
+    }
+
+    //@dev: verifies if the the friend exists
+    function doesFriendExist (address _address) private view returns (bool friendFound){
+        users memory currentUser = addressToUserMapping[msg.sender];
+        uint noOfFriends = currentUser.friends.length;
+        
+           //@dev: traverse through every element in a users's friend array, to verify if the address passed into this function exists
+            for (uint i=0; i<noOfFriends;i++){
+                if(currentUser.friends[i] == _address){
+                    friendFound = true;
+                    break;
+                }else{
+                    friendFound = false;
+                }
+            }
+        return friendFound;
+        
+    }
     
-    function addExpense(uint expenseAmount, address[] memory spenders) public{
+    //@dev: function to add expenses
+    function addExpense(uint expenseAmount, address[] memory spenders) public checkUserValidity() checkFriendList(spenders){
         expense memory currentExpense = expense(spenders,expenseAmount);
+        expenseMapper[transactionId++] = currentExpense;
         
     }
 }
