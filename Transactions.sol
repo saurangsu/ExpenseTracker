8df4b7b38987e2199762d3a3b7899047d5836a55
saurangsu <saurangsu.lahiri.88@gmail.com>
2019-03-12 22:17:29 -0400

    fixed issues with add expenses
    The issues w.r.t previous update have been avoided by changing the approach a little.

--- Transactions.sol
+++ Transactions.sol
@@ -1,14 +1,31 @@
+/*
+    @Author/dev: Functionalities of this contract - 
+    
+        1. Record an expense
+        2. Verify if expenses can only be added between friends
+        3. Include the following splitting mechanisms - Equal and shares
+        4. Delete an expense, ensuring that only the owner of 
+        
+    Work need to be done - 
+    
+        1. Add every expense to an array, to have a list of expenses - some logical errors happened while trying to implement that
+        2. Modify a transaction
+        
+        
+*/
+
 pragma solidity ^0.5.0;
 
 import "./User.sol";
 
 contract transactions is user{
     
-    //uint transactionId = 0;
+   // uint transactionId = 0;
+    
     
     enum ExpenseTypes {DEFAULT, EQUAL, PERCENTAGE, SHARE}
     ExpenseTypes expenseType;
-    
+    //uint transactionId;
     struct expense{
         
         address[] spenders;
@@ -17,9 +34,13 @@ contract transactions is user{
         address payer;
         uint[] distributedAmounts;
     }
-    uint[] expensesList;
-    mapping(uint => expense) expenseMapper;    
-
+    //expense[] expensesList;
+    mapping(string => expense) expenseMapper;    
+    
+/*    constructor() public{
+        transactionId = 0;
+    }*/
+    //uint transactionId = expensesList.length;
     //@dev: for every element in the _friends array, verify if that friend exists using FN doesFriendExist
     modifier checkFriendList(address[] memory _friends){
         users memory currentUser = addressToUserMapping[msg.sender];
@@ -44,6 +65,11 @@ contract transactions is user{
         }
 
     }
+    
+    modifier ownerOfTransaction(string memory _textDetails){
+        require(expenseMapper[_textDetails].payer == msg.sender);
+        _;
+    }
 
     //@dev: verifies if the the friend exists
     function doesFriendExist (address _address) private view returns (bool friendFound){
@@ -64,22 +90,22 @@ contract transactions is user{
     }
     
     //@dev: function to add expenses
-    function addExpense(uint expenseAmount, ExpenseTypes _type, address[] memory spenders, uint[] memory splits) public checkUserValidity() checkFriendList(spenders){
+    function addExpense(string memory txDetails, uint expenseAmount, ExpenseTypes _type, address[] memory spenders, uint[] memory splits) public checkUserValidity() checkFriendList(spenders){
        
         //uint[] memory splitAmount;
        
-        uint transactionId = expensesList.length;
-        expensesList.push(transactionId);
+        
+        //expensesList.push(transactionId);
         expense memory currentExpense = expense(spenders,expenseAmount, splits,msg.sender,new uint[](0));
         
-        expenseMapper[transactionId] = currentExpense;
+        expenseMapper[txDetails] = currentExpense;
         
  
         if (_type == ExpenseTypes.EQUAL){
            uint splitPerPerson = expenseAmount/spenders.length;
            for(uint i=0;i<spenders.length;i++){
                //splitAmount[i].push(splitPerPerson);
-               expenseMapper[transactionId].distributedAmounts.push(splitPerPerson);
+               expenseMapper[txDetails].distributedAmounts.push(splitPerPerson);
               }
 
        }else if(_type == ExpenseTypes.SHARE){
@@ -90,25 +116,30 @@ contract transactions is user{
            totalShares = totalShares + splits[i];
       
            }
-           
+           //uint temp = transactionId;
            for(uint i=0;i<splits.length;i++){
            
-           
-           expenseMapper[transactionId].distributedAmounts
+           //if(temp == 0 ){
+           expenseMapper[txDetails].distributedAmounts
            .push((expenseAmount/totalShares)*splits[i]);         
-           }
-
+           }//}
+            
+       }else{
+           
        }
 
-
+    //transactionId++;
     //transactionId = transactionId + 1;
-    
-
+    //expensesList.push(currentExpense);
+        //increaseTransactionID(transactionId);
 
     }
+    
+/*    function increaseTransactionID(uint _transactionId) private{
+        expensesList.push(_transactionId);
+    }*/
 
-
-    function showBalance() public returns(uint){
-        return addressToUserMapping[msg.sender].amountOwed;
+    function deleteTransaction(string memory _textDetails) public ownerOfTransaction(_textDetails){
+        delete expenseMapper[_textDetails];
     }
 }
